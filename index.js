b1b6f25fed078ac71a24d35f7dfc386695489bbe
Ziru Zou <jiyucoding@yahoo.com>
2023-01-22 21:46:10 -0500

    createEmployeeRecord function, return an object

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code here
+
+function createEmployeeRecord(arr) {
+
+    const firstName = arr[0]
+    const familyName = arr[1]
+    const title = arr[2]
+    const payPerHour = arr[3]
+
+    return {
+        firstName: firstName,
+        familyName: familyName,
+        title: title,
+        payPerHour: payPerHour,
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
